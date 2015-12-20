a4b3950322fbc7b05121ba3172bf666307b855f8
Jeff Ogata <jeffogata@outlook.com>
2015-12-20 17:51:43 -0600

    initial commit

---
+++ AspNetConfigurationVS/Startup.cs
@@ -0,0 +1,40 @@
+﻿namespace AspNetConfigurationVS
+{
+    using Microsoft.AspNet.Builder;
+    using Microsoft.AspNet.Http;
+    using Microsoft.Extensions.Configuration;
+
+    public class Startup
+    {
+        private readonly IConfigurationRoot _configuration;
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddXmlFile("appsettings.xml")
+                .AddEnvironmentVariables();
+
+            _configuration = builder.Build();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            var setting1 = _configuration["setting1"];
+            var setting2 = _configuration["setting2"];
+            var setting3 = _configuration["setting3"];
+            var commandTimeout = _configuration["data:commandTimeout"];
+            var defaultConnection = _configuration["data:connectionStrings:default"];
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync(
+                    $"<div>Setting 1 = {setting1}</div>" +
+                    $"<div>Setting 2 = {setting2}</div>" +
+                    $"<div>Setting 3 = {setting3}</div>" +
+                    $"<div>Command Timeout = {commandTimeout}</div>" +
+                    $"<div>Default Connection = {defaultConnection}</div>");
+            });
+        }
+    }
+}
